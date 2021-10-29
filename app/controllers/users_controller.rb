df877863b624dbb6f607173565b27b7f81c0cb76
Bruno Campos <bruno.mcamps@outlook.com>
2021-10-29 06:28:58 -0300

    Updated users controller, new.html.erb and created partial view to render errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,21 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new
+    @user = User.new(params[:user]) # not the final implementation!
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #handle a successful save
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
