757e81a32cb7e53cb1141691d327d8156da2cd8a
Bruno Campos <bruno.mcamps@outlook.com>
2021-10-31 16:36:13 -0300

    Updated sessions controller and new.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log in user and redirect to the user's show page
+    else
+      # create error nessage
+      # render 'new'
+    end
+
+  end
+
+  def destroy
+
+  end
 end
