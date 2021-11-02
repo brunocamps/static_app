31ed22f1649860039472bacc4a24b62ceb201771
Bruno Campos <bruno.mcamps@outlook.com>
2021-11-02 08:48:23 -0300

    Updated header, sessions controller, sessions helper, environment.js. Solved login/logout problem
    Created method log_out on SessionsHelper to allow logging out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,11 @@ class SessionsController < ApplicationController
       render 'new'
     end
 
+    def destroy
+      log_out
+      redirect_to root_url
+    end
+
   end
 
   def destroy
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,4 +11,15 @@ module SessionsHelper
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
+
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # Logs out the current user.
+  def log_out
+    reset_session
+    @current_user = nil
+  end
 end
